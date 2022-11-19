29bef8e0f9306f4094790defcab71335fa6fea78
Arturo Reyna <a.reyna@uniandes.edu.co>
2022-11-18 22:34:51 -0600

    Fixed SS issue

--- tests/version342.spec.ts
+++ tests/version342.spec.ts
@@ -11,14 +11,15 @@ test.beforeEach(async ({ page }) => {
 
 test('No debes ser capaz de crear un miembro sin email', async ({ page }) => {
     await page.click('#ember39');
+    await page.screenshot({path: 'screenshot-t/t2-1.png', fullPage: true});
     await page.click('#ember78');
-    await page.screenshot({path: 'screenshot/miembro1.png', fullPage: true});
+    await page.screenshot({path: 'screenshot-t/t2-2.png', fullPage: true});
     await page.locator('id=member-name').fill(faker.name.fullName());
-    await page.screenshot({path: 'screenshot/miembro2.png', fullPage: true});
+    await page.screenshot({path: 'screenshot-t/t2-3.png', fullPage: true});
     await page.click('text=Save');
-    await page.screenshot({path: 'screenshot/miembro3.png', fullPage: true});
+    await page.screenshot({path: 'screenshot-t/t2-3.png', fullPage: true});
     await page.goto('http://localhost:3001/ghost/#/members');
-    await page.screenshot({path: 'screenshot/miembro4.png', fullPage: true});
+    await page.screenshot({path: 'screenshot-t/t2-4.png', fullPage: true});
     await expect(page.getByText('Leave')).toBeFalsy;
     
 });
@@ -26,27 +27,27 @@ test('No debes ser capaz de crear un miembro sin email', async ({ page }) => {
 test('Debes ser capaz de crear un miembro con nombre y email', async ({ page }) => {
     await page.click('#ember39');
     await page.click('#ember78');
-    await page.screenshot({path: 'screenshot/miembro17.png', fullPage: true});
+    await page.screenshot({path: 'screenshot-t/t1-1.png', fullPage: true});
     await page.locator('id=member-name').fill(faker.name.findName());
-    await page.screenshot({path: 'screenshot/miembro18.png', fullPage: true});
+    await page.screenshot({path: 'screenshot-t/t1-2.png', fullPage: true});
     await page.locator('id=member-email').fill(faker.internet.email());
-    await page.screenshot({path: 'screenshot/miembro19.png', fullPage: true});
+    await page.screenshot({path: 'screenshot-t/t1-3.png', fullPage: true});
     await page.click('text=Save');
-    await page.screenshot({path: 'screenshot/miembro20.png', fullPage: true});
+    await page.screenshot({path: 'screenshot-t/t1-4.png', fullPage: true});
     await page.goto('http://localhost:3001/ghost/#/members');
-    await page.screenshot({path: 'screenshot/miembro21.png', fullPage: true});
+    await page.screenshot({path: 'screenshot-t/t1-5.png', fullPage: true});
     await expect(page.getByText('usuario@uniandes.edu.co')).toBeTruthy();
-    await page.screenshot({path: 'screenshot/miembro22.png', fullPage: true});
+    await page.screenshot({path: 'screenshot-t/t1-6.png', fullPage: true});
   });
 
 test('No debes ser capaz de dejar vacio el autor de un post', async ({ page }) => {
     await page.goto('http://localhost:3001/ghost/#/posts');
     await page.click('#ember124');
-    await page.screenshot({path: 'screenshot/post1.png', fullPage: true});
+    await page.screenshot({path: 'screenshot-t/t3-1.png', fullPage: true});
     await page.click('[title=Settings]');
-    await page.screenshot({path: 'screenshot/post2.png', fullPage: true});
+    await page.screenshot({path: 'screenshot-t/t3-2.png', fullPage: true});
     await page.click('[aria-label="remove element"]');
-    await page.screenshot({path: 'screenshot/post3.png', fullPage: true});
+    await page.screenshot({path: 'screenshot-t/t3-3.png', fullPage: true});
     await expect(page.getByText('At least one author is required')).toBeTruthy();
-    await page.screenshot({path: 'screenshot/post4.png', fullPage: true});
+    await page.screenshot({path: 'screenshot-t/t3-4.png', fullPage: true});
 });
